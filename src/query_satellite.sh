bcdefa6f4fd7631444eb0407010b8f0d66f9e4fc
lisaargento <116157281+lisaargento@users.noreply.github.com>
2022-12-14 15:49:24 +0100

    Update query_satellite.sh

--- src/query_satellite.sh
+++ src/query_satellite.sh
@@ -3,3 +3,10 @@
 for region in "frh01" "frh02" "frh03" "frh04"; do
 python PIR-Breizhcrops/src/query_gee.py /data2/france2018/${region}.shp --start 2018-01-01 --end 2018-12-31 --label-col CODE_CULTU --id-col ID_PARCEL --outfolder /data2/france2018/csv/$region
 done
+
+# for %x in "frh01" "frh02" "frh03" "frh04" do python C:/Users/Formation/Desktop/PIR_Breizhcrops/processing/query_gee.py C:/Users/Formation/Desktop/PIR_Breizhcrops/data/2019/%x.shp --start 2019-01-01 --end 2019-12-31 --label-col CODE_CULTU --id-col ID_PARCEL --outfolder C:/Users/Formation/Desktop/PIR_Breizhcrops/data/csv/%x.csv
+
+# for region in "frh01" "frh02" "frh03" "frh04"; do python home/Formation/Desktop/PIR_Breizhcrops/processing/query_gee.py /home/lisa_argento/Formation/Desktop/PIR_Breizhcrops/data/2019/{}.shp --start 2019-01-01 --end 2019-12-31 --label-col CODE_CULTU --id-col ID_PARCEL --outfolder C:/Users/Formation/Desktop/PIR_Breizhcrops/data/csv/$region
+# done
+
+# python C:/Users/Formation/Desktop/PIR_Breizhcrops/processing/query_gee.py C:/Users/Formation/Desktop/PIR_Breizhcrops/data/2019/frh01.shp --start 2019-01-01 --end 2019-12-31 --label-col CODE_CULTU --id-col ID_PARCEL --outfolder C:/Users/Formation/Desktop/PIR_Breizhcrops/data/csv/frh01.csv
